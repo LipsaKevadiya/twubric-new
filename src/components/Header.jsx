be41e86697beb283e3eb45169cf05d61655bfc85
lipsa <lipsa.kevadiya@exhibytesolution.com>
2024-03-18 12:08:52 +0530

    Twubric app in ReactJS with date sorting.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Header() {
+function Header({ startDate, endDate, setStartDate, setEndDate }) {
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  };
   return (
-    <div>
-      <div className="mt-4 px-5">
-        <div className="row mb-3">
-          <div className="col-md-6" style={{ color: "darkslategrey" }}>
+    <div className="ml-6">
+      <div className="container mt-4 px-2">
+        <div className="row mb-3 ">
+          <div className="col-md-6 " style={{ color: "darkslategrey" }}>
             <div>
-              <h1 style={{ color: "red" }}>Twubric</h1>
+              <h1 className="text-danger">Twubric</h1>
             </div>
             <label for="dateFilter">Joined Twitter between:</label>
-          
-            <label for="startDate" className="date" style={{ color: "red" }}>
+
+            <label for="startDate" className="date text-danger">
               Start Date:
             </label>
-            <input type="date" id="startDate" placeholder="YYYY-MM-DD" />
-            <label for="endDate" className="date" style={{ color: "red" }}>
+            <input
+              type="date"
+              id="startDate"
+              placeholder="YYYY-MM-DD"
+              value={startDate}
+              onChange={handleStartDateChange}
+            />
+            <label for="endDate" className="date text-danger">
               End Date:
             </label>
-            <input type="date" id="endDate" placeholder="YYYY-MM-DD" />
+            <input
+              type="date"
+              id="endDate"
+              placeholder="YYYY-MM-DD"
+              value={endDate}
+              onChange={handleEndDateChange}
+            />
           </div>
         </div>
-
-     
       </div>
     </div>
   );
